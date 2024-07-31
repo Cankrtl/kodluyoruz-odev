f53a178af16c0c5f2e489d1a2935981b7b85f5d6
Can Kartal <canskartal@hotmail.com>
2024-07-31 23:38:49 +0300

    commit

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 let fullName = prompt("Lutfen Adinizi Giriniz: ")
-
-
-
 let myName = document.querySelector("#name")
 myName.innerHTML = fullName
-
 let myClock = document.querySelector("#myClock")
 
 function dateOclock() {
-    var today = new Date() 
-    var day =today.toLocaleString('tr-TR', {weekday: 'long'})
-    var time = today.toLocaleTimeString('tr-TR') 
+    let today = new Date() 
+    let day = today.toLocaleString('tr-TR', {weekday: 'long'})
+    let time = today.toLocaleTimeString('tr-TR') 
     document.getElementById("myClock").innerHTML = time + " "+day; 
 }
 setInterval(dateOclock(), 1000); 
